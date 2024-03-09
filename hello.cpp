ccad5911c6862ae4b8582c7ca6f46763c1128c7b
Atif <91662836+mo-atiff@users.noreply.github.com>
2024-03-09 10:40:23 +0530

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
+//File: hello.cpp
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
 }
